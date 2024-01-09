596a74047c946fd39659179f74e0d824d3a48e3f
aekmekci72 <anna.ekmekci@gmail.com>
2024-01-08 21:49:37 -0500

    server stuff, but it doesn't really work yet

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,9 +1,27 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<!-- src/routes/+page.svelte -->
+<script>
+    export let data;
+    console.log(data);
+  </script>
+  
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  {#if data && data.length > 0}
+    <ul>
+      {#each data as item (item.id)}
+        <li>{item.name}</li>
+      {/each}
+    </ul>
+  {:else}
+    
+    <p>No data available</p>
+  {/if}
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
+  
