8ab547f321dc42fd97518f25c1b60682815dbdd4
shreejalearn <shreeja.das.16@gmail.com>
2024-01-10 19:07:32 -0500

    small changes

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,9 +1,18 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
+<h1 class="text-3xl font-bold underline custom-font">
+  Rate Mate
 </h1>
 
 <style lang="postcss">
+    @font-face {
+    font-family: 'EyesomeScript';
+    src: url('EyesomeScript.otf') format('opentype');
+  }
+
+  .custom-font {
+    font-family: 'EyesomeScript', sans-serif;
+  }
+
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: #FFFBF6;
   }
 </style>
